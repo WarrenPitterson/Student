b5d666d664efef358120a794f9a851020f4d7ad0
warren pitterson <warrenpitterson@gmail.com>
2019-06-20 17:39:37 +0100

    Add project files.

---
+++ Student/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date1 = new DateTime(2019, 06, 20);
+
+            Console.WriteLine(date1.ToString("dd/MM/yyyy"));
+            
+        }
+    }
+}
---
+++ Student/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Student
+    {
+        public string name { get; set; }
+        public int semester { get; set; }
+        public int rollNumber { get; set; }
+        public DateTime dateOfAdmission { get; set; }
+
+        public Student()
+        {
+            this.name = "student";
+            this.semester = 1;
+            this.rollNumber = 0000;
+            this.dateOfAdmission = dateOfAdmission;
+        }
+
+        public Student(string name, int semester, int rollNumber, DateTime dateOfAdmission)
+        {
+            this.name = name;
+            this.semester = semester;
+            this.rollNumber = rollNumber;
+            this.dateOfAdmission = dateOfAdmission;
+        }
+    }
+}
